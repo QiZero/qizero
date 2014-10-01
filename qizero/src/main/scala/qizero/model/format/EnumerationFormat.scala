dc5e9a32e0c0111aefd594d28b5f4b77a9ebd2c1
Ricardo Franco <rmf.franco@gmail.com>
2014-10-01 08:58:11 -0300

    improve mapper and formats

--- qizero/src/main/scala/qizero/model/format/EnumerationFormat.scala
+++ qizero/src/main/scala/qizero/model/format/EnumerationFormat.scala
@@ -3,35 +3,42 @@ package qizero.model.format
 import play.api.libs.json._
 
 trait EnumerationFormat {
-  protected final def enumReads[E <: Enumeration](enum: E): Reads[E#Value] =
-    new Reads[E#Value] {
-      def reads(json: JsValue): JsResult[E#Value] = json match {
-        case JsString(s) => {
-          try {
-            JsSuccess(enum.withName(s))
-          } catch {
-            case _: NoSuchElementException =>
-              JsError(s"Enumeration expected of type: '${enum.getClass}', but it does not appear to contain the value: '$s'")
-          }
-        }
-        case JsNumber(id) => {
-          try {
-            JsSuccess(enum(id.intValue()))
-          } catch {
-            case _: NoSuchElementException =>
-              JsError(s"Enumeration expected of type: '${enum.getClass}', but it does not appear to contain the value: '$id'")
-          }
-        }
-        case _ => JsError("String value expected")
+
+  protected final def enumValueReads[E <: Enumeration](enum: E): Reads[E#Value] = Reads {
+    case JsString(s) =>
+      try {
+        JsSuccess(enum.withName(s))
+      } catch {
+        case _: NoSuchElementException =>
+          JsError(s"Enumeration expected of type: '${enum.getClass}', but it does not appear to contain the value: '$s'")
       }
-    }
+    case _ => JsError("String value expected")
+  }
 
-  protected final def enumWrites[E <: Enumeration](useId: Boolean): Writes[E#Value] =
-    new Writes[E#Value] {
-      def writes(v: E#Value): JsValue = if (useId) JsNumber(v.id) else JsString(v.toString)
-    }
+  protected final def enumIdReads[E <: Enumeration](enum: E): Reads[E#Value] = Reads {
+    case JsNumber(id) =>
+      try {
+        JsSuccess(enum(id.intValue()))
+      } catch {
+        case _: NoSuchElementException =>
+          JsError(s"Enumeration expected of type: '${enum.getClass}', but it does not appear to contain the value: '$id'")
+      }
+    case _ => JsError("String value expected")
+  }
 
-  protected final def enumFormat[E <: Enumeration](enum: E, useId: Boolean): Format[E#Value] = {
-    Format(enumReads(enum), enumWrites(useId))
+  protected final def enumValueWrites[E <: Enumeration](): Writes[E#Value] = {
+    Writes { v: E#Value => JsString(v.toString)}
+  }
+
+  protected final def enumIdWrites[E <: Enumeration](): Writes[E#Value] = {
+    Writes { v: E#Value => JsNumber(v.id)}
+  }
+
+  protected final def enumValueFormat[E <: Enumeration](enum: E): Format[E#Value] = {
+    Format(enumValueReads(enum), enumValueWrites())
+  }
+
+  protected final def enumIdFormat[E <: Enumeration](enum: E): Format[E#Value] = {
+    Format(enumIdReads(enum), enumIdWrites())
   }
 }
--- qizero/src/main/scala/qizero/persistence/Mappers.scala
+++ qizero/src/main/scala/qizero/persistence/Mappers.scala
@@ -1,6 +1,7 @@
 package qizero.persistence
 
 import java.sql._
+import scala.slick.jdbc.{GetResult, SetParameter}
 import scala.util.Try
 import org.joda.time._
 
@@ -9,15 +10,22 @@ trait DateMapper {
 
   import profile.simple._
 
-  implicit val dateTimeMapper = MappedColumnType.base[DateTime, Timestamp](
+  implicit val GetDateTime = GetResult[DateTime](r => new DateTime(r.nextTimestamp.getTime))
+  implicit val SetDateTime = SetParameter[DateTime]((t, p) => p.setTimestamp(new Timestamp(t.getMillis)))
+
+  implicit val DateTimeMapper = MappedColumnType.base[DateTime, Timestamp](
     dateTime => new Timestamp(dateTime.getMillis),
     timestamp => new DateTime(timestamp.getTime)
   )
 
-  implicit val localDateMapper = MappedColumnType.base[LocalDate, Date](
+  implicit val GetLocalDate = GetResult[LocalDate](r => new LocalDate(r.nextDate.getTime))
+  implicit val SetLocalDate = SetParameter[LocalDate]((t, p) => p.setDate(new Date(t.toDate.getTime)))
+
+  implicit val LocalDateMapper = MappedColumnType.base[LocalDate, Date](
     dt => new Date(dt.toDate.getTime),
     d => new LocalDate(d.getTime)
   )
+
 }
 
 trait EnumerationMapper {
@@ -25,12 +33,12 @@ trait EnumerationMapper {
 
   import profile.simple._
 
-  protected final def enumStringMapper(enum: Enumeration) = MappedColumnType.base[enum.Value, String](
+  protected final def enumValueMapper(enum: Enumeration) = MappedColumnType.base[enum.Value, String](
     e => e.toString,
     s => Try(enum.withName(s)).getOrElse(throw new IllegalArgumentException(s"enumeration $s doesn't exist $enum [${enum.values.mkString(",")}]"))
   )
 
-  protected final def enumIntMapper(enum: Enumeration) = MappedColumnType.base[enum.Value, Int](
+  protected final def enumIdMapper(enum: Enumeration) = MappedColumnType.base[enum.Value, Int](
     e => e.id,
     i => enum.apply(i)
   )
